957d153e869dd2527ff4c4878c28f941de96857a
JeromeB <bachjerome3@gmail.com>
2021-07-21 15:57:20 +0200

    add type for form

--- src/Form/SubjectType.php
+++ src/Form/SubjectType.php
@@ -4,8 +4,10 @@ namespace App\Form;
 
 use App\Entity\Subject;
 use App\Entity\Tags;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,8 +16,8 @@ class SubjectType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('content')
+            ->add('title', TextType::class)
+            ->add('content', CKEditorType::class)
             ->add('tags', EntityType::class, [
                 'class' => Tags::class,
                 'choice_label' => 'name',
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,7 +3,10 @@
 namespace App\Form;
 
 use App\Entity\User;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,14 +15,14 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
-            ->add('lastname')
-            ->add('firstname')
-            ->add('description')
-            ->add('email')
-            ->add('discord')
-            ->add('twitchChannel')
-            ->add('city')
+            ->add('username', TextType::class)
+            ->add('lastname', TextType::class)
+            ->add('firstname', TextType::class)
+            ->add('description', CKEditorType::class)
+            ->add('email', EmailType::class)
+            ->add('discord', TextType::class)
+            ->add('twitchChannel', TextType::class)
+            ->add('city', TextType::class)
         ;
     }
 
