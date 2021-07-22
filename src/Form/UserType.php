06a6aa2d23f4db3206a5f33a6ee8f6dc6868c99b
JeromeB <bachjerome3@gmail.com>
2021-07-22 10:48:47 +0200

    layout form user

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -15,14 +15,23 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username', TextType::class)
-            ->add('lastname', TextType::class)
-            ->add('firstname', TextType::class)
-            ->add('description', CKEditorType::class)
-            ->add('email', EmailType::class)
-            ->add('discord', TextType::class)
-            ->add('twitchChannel', TextType::class)
-            ->add('city', TextType::class)
+            ->add('username', TextType::class, ['label' => false])
+            ->add('lastname', TextType::class, ['label' => false])
+            ->add('firstname', TextType::class, ['label' => false])
+            ->add('description', CKEditorType::class, [
+                'label' => false,
+                'required' => false
+            ])
+            ->add('email', EmailType::class, ['label' => false])
+            ->add('discord', TextType::class, [
+                'label' => false,
+                'required' => false
+            ])
+            ->add('twitchChannel', TextType::class, [
+                'label' => false,
+                'required' => false
+            ])
+            ->add('city', TextType::class, ['label' => false])
         ;
     }
 
