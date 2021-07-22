5be6fce46bbe849a1301330c5be518ab7da49a9f
JeromeB <bachjerome3@gmail.com>
2021-07-22 09:01:48 +0200

    ajout navbar +footer

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin", name="dashboard_")
+ */
+class DashboardController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('admin/dashboard.html.twig');
+    }
+}
